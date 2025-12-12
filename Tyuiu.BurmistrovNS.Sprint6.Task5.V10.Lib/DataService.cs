69dd53620cafbc735e28d1e4d01f8bdbba7ac135
Николай Бурмистров <Tyuiu.BurmistrovNS@outlook.com>
2025-12-13 01:01:39 +0500

    Task5

---
+++ Tyuiu.BurmistrovNS.Sprint6.Task5.V10.Lib/DataService.cs
@@ -0,0 +1,16 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+
+namespace Tyuiu.BurmistrovNS.Sprint6.Task5.V10.Lib
+{
+    public class DataService : ISprint6Task5V10
+    {
+        public int len = 25;
+        public double[] LoadFromDataFile(string path)
+        {
+            string[] lines = File.ReadAllLines(path);
+            double[] numsArray = lines.Select(line => Convert.ToDouble(line)).ToArray();
+            numsArray = numsArray.Where(val => val != 0).ToArray();
+            return numsArray;
+        }
+    }
+}
---
+++ Tyuiu.BurmistrovNS.Sprint6.Task5.V10/Form1.cs
@@ -0,0 +1,84 @@
+using Tyuiu.BurmistrovNS.Sprint6.Task5.V10.Lib;
+namespace Tyuiu.BurmistrovNS.Sprint6.Task5.V10
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        DataService ds = new DataService();
+
+        string path = @"C:\Users\Николай\source\repos\Tyuiu.BurmistrovNS.Sprint6\Tyuiu.BurmistrovNS.Sprint6.Task5.V10\bin\Debug\net8.0-windows\OutPutFileTask5.txt";
+
+        private void buttonRunClick(object sender, EventArgs e)
+        {
+            try
+            {
+                dataGridViewNums.ColumnCount = 2;
+                dataGridViewNums.Columns[0].Width = 20;
+                dataGridViewNums.Columns[1].Width = 50;
+
+                double[] numsMass = new double[ds.len];
+                numsMass = ds.LoadFromDataFile(path);
+
+                for (int i = 0; i < numsMass.Length; i++)
+                {
+                    dataGridViewNums.Rows.Add(Convert.ToString(i), Convert.ToString(numsMass[i]));
+
+                }
+
+            }
+            catch
+            {
+                MessageBox.Show("Введены неверные данные", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void groupBox3_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonHelp_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Таск 5 выполнил студент группы ИСТНб-25-1 Бурмистров Николай Сергеевич ", "Справка", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+
+        private void buttonHelp_MouseEnter(object sender, MouseEventArgs e)
+        {
+            MessageBox.Show("Таск 5 выполнил студент группы ИСТНб-25-1 Бурмистров Николай Сергеевич ", "Справка", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                System.Diagnostics.Process txt = new System.Diagnostics.Process();
+                txt.StartInfo.FileName = "notepad.exe";
+                txt.StartInfo.Arguments = path;
+                txt.Start();
+            }
+            catch
+            {
+                MessageBox.Show("Введены неверные данные", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

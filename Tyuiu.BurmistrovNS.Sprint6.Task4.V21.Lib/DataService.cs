42ffa3338e24fb8902d970e8f907e7599df2deb5
Николай Бурмистров <Tyuiu.BurmistrovNS@outlook.com>
2025-12-12 23:59:54 +0500

    Task4

---
+++ Tyuiu.BurmistrovNS.Sprint6.Task4.V21.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+namespace Tyuiu.BurmistrovNS.Sprint6.Task4.V21.Lib
+{
+    public class DataService : ISprint6Task4V21
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            int len = (stopValue - startValue) + 1;
+            double[] valueArray = new double[len];
+            double y;
+            for (int x = startValue, count = 0; x <= stopValue; x++, count++)
+            {
+                y = Math.Round((2 * Math.Cos(x) + 2) / (2 * x - 1) + Math.Cos(x) - 5 * x + 3, 2);
+                valueArray[count] = y;
+
+            }
+            return valueArray;
+        }
+    }
+}
